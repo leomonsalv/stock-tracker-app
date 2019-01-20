664abca56ff92867f370bf8e590c17d729ef4172
Leonardo Monsalve <leonardomonsalv@gmail.com>
2019-01-20 02:50:34 +0000

    Create friendship model and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+  get 'my_friends', to: 'users#my_friends'
 end
